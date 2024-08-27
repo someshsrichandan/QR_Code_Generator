4e97c3b0cead9d6ca47e9a1f3878a17e627b7223
Somesh Srichandan Sahoo <69620426+someshsrichandan@users.noreply.github.com>
2024-08-27 06:58:37 +0000

    ui heading added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
 function App() {
   return (
-    <>
-    </>
+    <div className="w-full h-screen flex justify-center items-center bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900">
+    <div className="w-8/12 h-5/6 border border-white rounded-xl shadow-gradient">
+    <h1 className="font-sciFi pt-4 font-semibold tracking-widest	text-white text-center text-4xl">QRcode <br/><p className="text-sm text-center">generator</p></h1>
+
+    </div>
+  </div>
+  
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'gradient': '0 4px 20px rgba(0, 0, 0, 0.5), 0 0 15px rgba(0, 0, 0, 0.2)',
+      },
+      fontFamily: {
+        sciFi: ['Orbitron', 'sans-serif'], // Define the sci-fi font family
+      },
+    },
   },
   plugins: [],
 }
