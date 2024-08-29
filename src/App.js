f9e2010fd5b6a9d1750724663fa2ba0f87c94e35
Somesh Srichandan Sahoo <69620426+someshsrichandan@users.noreply.github.com>
2024-08-29 17:00:59 +0000

    some responsive fix

--- src/App.js
+++ src/App.js
@@ -23,13 +23,13 @@ function App() {
   };
 
   return (
-    <div className="w-full h-full min-h-screen flex flex-col justify-center items-center bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 p-4">
-      <div className="w-full max-w-3xl h-full max-h-[90vh] border border-white rounded-xl shadow-gradient p-4 flex flex-col gap-6">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 p-4">
+      <div className="w-full max-w-3xl h-full max-h-[90vh] border border-white rounded-xl shadow-gradient p-4 flex flex-col gap-6 overflow-hidden">
         <h1 className="font-sciFi pt-4 font-semibold tracking-widest text-white text-center text-3xl md:text-4xl">
           QRcode <br />
           <p className="text-sm text-center">generator</p>
         </h1>
-        <div className="flex flex-col-reverse md:flex-row justify-around gap-6 md:gap-10 p-4 flex-grow items-center">
+        <div className="flex flex-col-reverse md:flex-row justify-around gap-6 md:gap-10 p-4 flex-grow items-center overflow-auto">
           <QRCodeDisplay qrCodeData={qrCodeData} />
 
           <div className="w-full max-w-xs">
--- src/components/QRCodeDisplay.js
+++ src/components/QRCodeDisplay.js
@@ -21,7 +21,7 @@ const QRCodeDisplay = ({ qrCodeData }) => {
 
   return (
     <div className="flex flex-col items-center gap-4 p-2 md:p-4">
-      <div ref={qrRef} className="p-4 bg-white rounded-lg shadow-md max-w-full border border-gray-300">
+      <div ref={qrRef} className="p-4 bg-white rounded-lg shadow-md max-w-full border border-gray-300 flex justify-center items-center">
         <QRCode
           value={qrCodeData.value}
           size={256}
