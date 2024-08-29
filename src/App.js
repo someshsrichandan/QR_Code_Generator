2947789ee7db705e5c2869e4701bee25dedd8433
Somesh Srichandan Sahoo <69620426+someshsrichandan@users.noreply.github.com>
2024-08-29 18:40:05 +0000

    github pages added succesfully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BasicSettings from "./components/BasicSettings";
 import AdvancedSettings from "./components/AdvancedSettings";
 import QRCodeDisplay from "./components/QRCodeDisplay";
@@ -14,6 +14,11 @@ function App() {
     qrStyle: "dots",
   });
 
+  // Scroll to top when the component mounts
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const handleBasicSubmit = (url) => {
     setQrCodeData({ ...qrCodeData, value: url });
   };
@@ -23,15 +28,17 @@ function App() {
   };
 
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 p-4">
-      <div className="w-full max-w-3xl h-full max-h-[90vh] border border-white rounded-xl shadow-gradient p-4 flex flex-col gap-6 overflow-hidden">
+    <div className="w-full min-h-screen flex flex-col justify-start items-center bg-[conic-gradient(at_bottom_right,_var(--tw-gradient-stops))] from-slate-900 via-purple-900 to-slate-900 p-4 overflow-y-auto">
+      <div className="w-full max-w-3xl border border-white rounded-xl shadow-gradient p-4 md:pt-8 md:mt-14 flex flex-col gap-6 overflow-hidden">
         <h1 className="font-sciFi pt-4 font-semibold tracking-widest text-white text-center text-3xl md:text-4xl">
           QRcode <br />
           <p className="text-sm text-center">generator</p>
         </h1>
-        <div className="flex flex-col-reverse md:flex-row justify-around gap-6 md:gap-10 p-4 flex-grow items-center overflow-auto">
+        <div className="flex flex-col-reverse md:flex-row justify-around gap-6 md:gap-10 p-4 items-center">
+          {/* QR Code Display Section */}
           <QRCodeDisplay qrCodeData={qrCodeData} />
 
+          {/* Input Fields Section */}
           <div className="w-full max-w-xs">
             <div className="flex justify-center mb-4">
               <div className="flex divide-x divide-gray-800">
